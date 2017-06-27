861353d65cedbdd8165576998a1e335ab4874cd9
Thomas Huisking <tomasfaustin@gmail.com>
2017-06-27 09:12:12 -0700

    Update view, model, and styling

--- directory/app/models/job.rb
+++ directory/app/models/job.rb
@@ -1,8 +1,7 @@
 class Job < ApplicationRecord
 
   def self.search(search)
-    where("name ILIKE ?", "%#{search}%")
-    where("position ILIKE ?", "%#{search}%")
+    where("name LIKE ? OR position LIKE ?", "%#{search}%", "%#{search}%")
   end
 
 end
